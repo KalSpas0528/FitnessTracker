a9d6ae7744ad14aa8f38580d8c3492470ecdce59
KalSpas0528 <kaloyan_kj@yahoo.com>
2024-10-01 19:04:43 +0000

    js first test

---
+++ var.js
@@ -0,0 +1,5 @@
+document.querySelector("form").addEventListener("submit", function(event) {
+let exerciseName = document.getElementById("exercise-name").value;
+let sets = document.getElementById("sets").value;
+let reps = document.getElementById("reps").value;
+let weight = document.getElementById("weight").value;
