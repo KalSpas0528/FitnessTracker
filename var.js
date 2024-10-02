7d8fc81e42b1417bd346611d7f3ae0b4b7246ed5
KalSpas0528 <kaloyan_kj@yahoo.com>
2024-10-02 19:04:39 +0000

    js save data

--- var.js
+++ var.js
@@ -1,5 +1,8 @@
 document.querySelector("form").addEventListener("submit", function(event) {
-let exerciseName = document.getElementById("exercise-name").value;
-let sets = document.getElementById("sets").value;
-let reps = document.getElementById("reps").value;
-let weight = document.getElementById("weight").value;
+    event.preventDefault(); 
+    let exerciseName = document.getElementById("exercise-name").value;
+    let sets = document.getElementById("sets").value;
+    let reps = document.getElementById("reps").value;
+    let weight = document.getElementById("weight").value;
+    console.log(`Exercise: ${exerciseName}, Sets: ${sets}, Reps: ${reps}, Weight: ${weight}`);
+});
