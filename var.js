50ed38df6ea0128443b304ccab2ce48c38412509
KalSpas0528 <kaloyan_kj@yahoo.com>
2024-10-04 18:58:04 +0000

    js formfunction

--- var.js
+++ var.js
@@ -4,6 +4,7 @@ document.querySelector("form").addEventListener("submit", function(event) {
     let sets = document.getElementById("sets").value;
     let reps = document.getElementById("reps").value;
     let weight = document.getElementById("weight").value;
+    let workoutDate = document.getElementById("workout-date").value; /
     console.log(`Exercise: ${exerciseName}, Sets: ${sets}, Reps: ${reps}, Weight: ${weight}`);
  
 let workoutList = document.getElementById("workout-list");
