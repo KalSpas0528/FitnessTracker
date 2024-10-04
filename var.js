f3c89434494a24aabaae3495ab24c0f7f13d7271
KalSpas0528 <kaloyan_kj@yahoo.com>
2024-10-04 19:25:32 +0000

    js fix errors

--- var.js
+++ var.js
@@ -4,9 +4,9 @@ document.querySelector("form").addEventListener("submit", function(event) {
     let sets = document.getElementById("sets").value;
     let reps = document.getElementById("reps").value;
     let weight = document.getElementById("weight").value;
-    let workoutDate = document.getElementById("workout-date").value; /
+    let workoutDate = document.getElementById("workout-date").value; 
     console.log(`Exercise: ${exerciseName}, Sets: ${sets}, Reps: ${reps}, Weight: ${weight}`);
- 
+
 let workoutList = document.getElementById("workout-list");
 let newWorkout = document.createElement("li");
 newWorkout.textContent = `Exercise: ${exerciseName}, Sets: ${sets}, Reps: ${reps}, Weight: ${weight} lbs`;
