640c30b8fc4dee0ea6d38f1401cc4c598c610b7c
KalSpas0528 <kaloyan_kj@yahoo.com>
2024-12-09 03:17:21 +0000

    fixed inspect elements on the website.

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ import { createClient } from '@supabase/supabase-js';
 import dotenv from 'dotenv';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import cors from 'cors';
+app.use(cors({ origin: 'https://kalspas0528.github.io' }));
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
